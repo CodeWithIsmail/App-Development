fb387d904e52032911b295d00903f52c2d68443e
CodeWithIsmail <hmismail360@gmail.com>
2024-06-12 00:49:45 +0600

    started app dev

---
+++ BMICalculator/app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.bmicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    TextView textView, editTextText3, textView3, textView2;
+    EditText editTextNumberSigned, editTextNumberDecimal, editTextNumberDecimal2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        editTextText3 = findViewById(R.id.editTextText3);
+        textView3 = findViewById(R.id.textView3);
+        textView2 = findViewById(R.id.textView2);
+        editTextNumberSigned = findViewById(R.id.editTextNumberSigned);
+        editTextNumberDecimal = findViewById(R.id.editTextNumberDecimal);
+        editTextNumberDecimal2 = findViewById(R.id.editTextNumberDecimal2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String wt = editTextNumberSigned.getText().toString();
+                String ht1 = editTextNumberDecimal.getText().toString();
+                String ht2 = editTextNumberDecimal2.getText().toString();
+
+                int weight = Integer.parseInt(wt);
+                int feet = Integer.parseInt(ht1);
+                int inch = Integer.parseInt(ht2);
+                double ht = feet * 0.3048 + inch * 0.0254;
+                double bmi = weight / (ht * ht);
+
+                textView2.setText(String.format("Your BMI is %.2f Kg/m^2", bmi));
+            }
+        });
+    }
+}
