40e3324541635cf0e1a4bfe135653746f7983e51
CodeWithIsmail <hmismail360@gmail.com>
2024-06-30 13:13:38 +0600

    quizz app created

--- quizz_app_flutter/lib/main.dart
+++ quizz_app_flutter/lib/main.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() => runApp(QuizApp());
 
@@ -43,34 +46,74 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  int currentQues = 0;
   List<Icon> scoreboard = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+  List<String> answers = ['false', 'true', 'true'];
+  bool done = false;
 
-  void checkAns() {
-    setState(() {
-      scoreboard.add(
-        Icon(
-          Icons.close,
-          color: Colors.red,
-          size: 30,
-        ),
-      );
-    });
+  void end() {
+    Fluttertoast.showToast(
+        msg: "No more questions!",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 3,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 20.0);
+  }
+
+  void CheckAns(String need) {
+    if (done)
+      end();
+    else {
+      setState(() {
+        if (answers[currentQues] == need) {
+          scoreboard.add(
+            Icon(
+              Icons.check,
+              color: Colors.green,
+              size: 30,
+            ),
+          );
+        } else {
+          scoreboard.add(
+            Icon(
+              Icons.close,
+              color: Colors.red,
+              size: 30,
+            ),
+          );
+        }
+        if (currentQues == 2) {
+          done = true;
+        } else
+          currentQues++;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
-      // mainAxisAlignment: ,
       children: [
         Expanded(
           flex: 5,
           child: Card(
-            color: Colors.blue,
-            child: Text(
-              'This is where the question text will go.',
-              style: TextStyle(
-                fontSize: 20,
+            color: Colors.blue.shade200,
+            child: SingleChildScrollView(
+              padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+              child: Text(
+                // textAlign: TextAlign.center,
+                questions[currentQues],
+                style: TextStyle(
+                  fontSize: 25,
+                ),
               ),
             ),
           ),
@@ -80,7 +123,7 @@ class _QuizPageState extends State<QuizPage> {
           child: Card(
             color: Colors.green,
             child: TextButton(
-              onPressed: checkAns,
+              onPressed: () => CheckAns('true'),
               child: Text(
                 'TRUE',
                 style: TextStyle(
@@ -96,7 +139,7 @@ class _QuizPageState extends State<QuizPage> {
           child: Card(
             color: Colors.red,
             child: TextButton(
-              onPressed: () {},
+              onPressed: () => CheckAns('false'),
               child: Text(
                 'FALSE',
                 style: TextStyle(
