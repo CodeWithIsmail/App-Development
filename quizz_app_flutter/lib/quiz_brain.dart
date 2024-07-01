40325b487ff753497df221bd9dca0556ea2d4502
CodeWithIsmail <hmismail360@gmail.com>
2024-07-01 10:56:00 +0600

    oop concept applied

--- quizz_app_flutter/lib/quiz_brain.dart
+++ quizz_app_flutter/lib/quiz_brain.dart
@@ -1,9 +1,11 @@
 import 'questionbank.dart';
 class QuizBrain {
-  List<Questionbank> questionbank = [
+  List<Questionbank> _questionbank = [
     Questionbank('You can lead a cow down stairs but not up stairs.', 'false'),
     Questionbank(
         'Approximately one quarter of human bones are in the feet.', 'true'),
     Questionbank('A slug\'s blood is green.', 'true')
   ];
+
+  List<Questionbank> get questionbank => _questionbank;
 }
