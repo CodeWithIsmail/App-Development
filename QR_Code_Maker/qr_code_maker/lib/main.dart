45eed844ddcb6d20fad2b807a7d31284697df6bd
CodeWithIsmail <hmismail360@gmail.com>
2024-07-04 01:20:58 +0600

    QR maker completed

--- QR_Code_Maker/qr_code_maker/lib/main.dart
+++ QR_Code_Maker/qr_code_maker/lib/main.dart
@@ -4,12 +4,22 @@ import 'package:flutter/material.dart';
 import 'constants.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
+String text = '';
+
 void main() {
-  runApp(const MyApp());
+  runApp(QRmaker());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class QRmaker extends StatefulWidget {
+  const QRmaker({super.key});
+
+  @override
+  State<QRmaker> createState() => _QRmakerState();
+}
+
+class _QRmakerState extends State<QRmaker> {
+  @override
+  final TextEditingController _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -24,18 +34,70 @@ class MyApp extends StatelessWidget {
           ),
           backgroundColor: Colors.brown.shade700,
         ),
-        body: QrImageView(
-          data: 'Name: Ismail Hossain'
-              ' Blood group: B+'
-              '  BMI: 21.10\n',
-          version: QrVersions.auto,
-          size: 200.0,
-          errorCorrectionLevel: QrErrorCorrectLevel.H,
-          backgroundColor: Colors.white,
-          embeddedImage: AssetImage(
-            'images/img.jpg',
-          ),
-          embeddedImageStyle: QrEmbeddedImageStyle(size: Size(35, 35)),
+        body: Column(
+          children: [
+            Card(
+              margin: EdgeInsets.all(25),
+              child: TextField(
+                minLines: 5,
+                maxLines: 6,
+                controller: _controller,
+                decoration: InputDecoration(
+                  hintText: 'Enter your text here',
+                  hintStyle: TextStyle(
+                    fontSize: 20,
+                  ),
+                  border: OutlineInputBorder(),
+                ),
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            TextButton(
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all(Colors.white),
+              ),
+              onPressed: () {
+                setState(() {
+                  text = _controller.text;
+                });
+              },
+              child: Text(
+                'SUBMIT',
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Expanded(
+              flex: 3,
+              child: QrImageView(
+                data: text,
+                version: QrVersions.auto,
+                size: 300.0,
+                errorCorrectionLevel: QrErrorCorrectLevel.H,
+                backgroundColor: Colors.white,
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                alignment: Alignment.bottomCenter,
+                child: Text(
+                  'Developed by CodeWithIsmail',
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
