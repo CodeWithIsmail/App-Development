c957777bc0ebeb14e5bf785784b444a686d9f358
CodeWithIsmail <hmismail360@gmail.com>
2024-07-13 17:48:08 +0600

    to do app ui created

---
+++ To Do App/to_do_app/lib/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const heading1style=TextStyle(
+  fontWeight: FontWeight.w900,
+  fontSize: 55,
+  color: Colors.white,
+);
+
+const heading6style=TextStyle(
+  // fontWeight: FontWeight.w900,
+  fontSize: 20,
+  color: Colors.white,
+);
