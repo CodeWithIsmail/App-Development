0a326dc2b7e3d333aa614dd488b5366f429e6a7d
CodeWithIsmail <hmismail360@gmail.com>
2024-09-15 14:20:02 +0600

    ocr

---
+++ OCR/lib/ImportAll.dart
@@ -0,0 +1,8 @@
+export 'dart:async';
+export 'dart:developer' hide Flow;
+export 'dart:io';
+
+export 'package:flutter/material.dart';
+export 'Homepage.dart';
+export 'package:gallery_picker/gallery_picker.dart';
+export 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
